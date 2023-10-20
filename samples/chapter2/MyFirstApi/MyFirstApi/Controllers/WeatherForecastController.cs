253ca776fc0332193ca27a6515d1a629bcaf2d83
Xiaodi Yan <yan_xiaodi@hotmail.com>
2023-10-21 10:48:58 +1300

    Update sample code of MyFirstApi

--- samples/chapter2/MyFirstApi/MyFirstApi/Controllers/WeatherForecastController.cs
+++ samples/chapter2/MyFirstApi/MyFirstApi/Controllers/WeatherForecastController.cs
@@ -9,7 +9,7 @@ public class WeatherForecastController : ControllerBase
   private static readonly string[] Summaries = new[]
   {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+  };
 
   private readonly ILogger<WeatherForecastController> _logger;
 
