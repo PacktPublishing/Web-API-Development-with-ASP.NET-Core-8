b2ec04d3c6f96e55c05668c167876e21ebfd8471
Xiaodi Yan <yan_xiaodi@hotmail.com>
2023-10-23 14:44:44 +1300

    Update BasicEfCoreDemo

--- samples/chapter5/BasicEfCoreDemo/Controllers/InvoicesController.cs
+++ samples/chapter5/BasicEfCoreDemo/Controllers/InvoicesController.cs
@@ -1,5 +1,6 @@
 using BasicEfCoreDemo.Data;
 using BasicEfCoreDemo.Models;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
@@ -11,147 +12,47 @@ namespace BasicEfCoreDemo.Controllers
     public class InvoicesController : ControllerBase
     {
         private readonly InvoiceDbContext _context;
-        private readonly ILogger<InvoicesController> _logger;
 
-        public InvoicesController(InvoiceDbContext context, ILogger<InvoicesController> logger)
+        public InvoicesController(InvoiceDbContext context)
         {
             _context = context;
-            _logger = logger;
         }
 
+        // The following code snippet shows how to retrieve all invoices from the database.
         // GET: api/Invoices
+        // [HttpGet]
+        // public async Task<ActionResult<IEnumerable<Invoice>>> GetInvoices()
+        // {
+        //     if (_context.Invoices == null)
+        //     {
+        //         return NotFound();
+        //     }
+        //     return await _context.Invoices.ToListAsync();
+        // }
+
+        // The following code snippet shows how to retrieve all invoices from the database with a specific status.
+        // [HttpGet]
+        // public async Task<ActionResult<IEnumerable<Invoice>>> GetInvoices(InvoiceStatus status)
+        // {
+        //     // Omitted for brevity 
+        //     return await _context.Invoices.Where(x => x.Status == status).ToListAsync();
+        // }
+
+        // The following code snippet shows how to retrieve pages of invoices from the database.s
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Invoice>>> GetInvoices(int page = 1, int pageSize = 10,
-            InvoiceStatus? status = null)
+        public async Task<ActionResult<IEnumerable<Invoice>>> GetInvoices(int page = 1, int pageSize = 10, InvoiceStatus? status = null)
         {
             if (_context.Invoices == null)
             {
                 return NotFound();
             }
-            // Use IQueryable
-            _logger.LogInformation($"Creating the IQueryable...");
-            var list1 = _context.Invoices.Where(x => status == null || x.Status == status);
-            _logger.LogInformation($"IQueryable created");
-            _logger.LogInformation($"Query the result using IQueryable...");
-            var query1 = list1.OrderByDescending(x => x.InvoiceDate)
-                .Skip((page - 1) * pageSize)
-                .Take(pageSize);
-            _logger.LogInformation($"Execute the query using IQueryable");
-            var result1 = await query1.ToListAsync();
-            _logger.LogInformation($"Result created using IQueryable");
-
-            // Use IEnumerable
-            _logger.LogInformation($"Creating the IEnumerable...");
-            var list2 = _context.Invoices.Where(x => status == null || x.Status == status).AsEnumerable();
-            _logger.LogInformation($"IEnumerable created");
-            _logger.LogInformation($"Query the result using IEnumerable...");
-            var query2 = list2.OrderByDescending(x => x.InvoiceDate)
-                .Skip((page - 1) * pageSize)
-                .Take(pageSize);
-            _logger.LogInformation($"Execute the query using IEnumerable");
-            var result2 = query2.ToList();
-            _logger.LogInformation($"Result created using IEnumerable");
-
-            return result1;
+            // The AsQueryable() method is not required, as the DbSet<TEntity> class implements the IQueryable<TEntity> interface.
+            return await _context.Invoices.AsQueryable().Where(x => status == null || x.Status == status)
+                        .OrderByDescending(x => x.InvoiceDate)
+                        .Skip((page - 1) * pageSize)
+                        .Take(pageSize)
+                        .ToListAsync();
         }
-
-        [HttpGet]
-        [Route("search")]
-        public async Task<ActionResult<IEnumerable<Invoice>>> SearchInvoices(string search)
-        {
-            if (_context.Invoices == null)
-            {
-                return NotFound();
-            }
-
-            var list = await _context.Invoices
-                // The below code will throw an exception if the CalculatedTax method is not static
-                //.Where(x => (x.ContactName.Contains(search) || x.InvoiceNumber.Contains(search)) && CalculateTax(x.Amount) > 10)
-                .Where(x => (x.ContactName.Contains(search) || x.InvoiceNumber.Contains(search)))
-                .Select(x => new Invoice
-                {
-                    Id = x.Id,
-                    InvoiceNumber = x.InvoiceNumber,
-                    ContactName = x.ContactName,
-                    // The below conversion will be executed on the client side
-                    Description = $"Tax: ${CalculateTax(x.Amount)}. {x.Description}",
-                    Amount = x.Amount,
-                    InvoiceDate = x.InvoiceDate,
-                    DueDate = x.DueDate,
-                    Status = x.Status
-                })
-                .ToListAsync();
-            return list;
-        }
-
-        private static decimal CalculateTax(decimal amount)
-        {
-            return amount * 0.15m;
-        }
-
-        [HttpGet]
-        [Route("paid")]
-        public async Task<ActionResult<IEnumerable<Invoice>>> GetPaidInvoices()
-        {
-            if (_context.Invoices == null)
-            {
-                return NotFound();
-            }
-
-            var list = await _context.Invoices
-                .FromSql($"SELECT * FROM Invoices WHERE Status = 2")
-                .ToListAsync();
-            return list;
-        }
-
-        [HttpGet]
-        [Route("status")]
-        public async Task<ActionResult<IEnumerable<Invoice>>> GetInvoices(string status)
-        {
-            if (_context.Invoices == null)
-            {
-                return NotFound();
-            }
-
-            var list = await _context.Invoices
-                .FromSql($"SELECT * FROM Invoices WHERE Status = {status}")
-                .ToListAsync();
-            return list;
-        }
-
-        [HttpGet]
-        [Route("free-search")]
-        public async Task<ActionResult<IEnumerable<Invoice>>> GetInvoices(string propertyName, string propertyValue)
-        {
-            if (_context.Invoices == null)
-            {
-                return NotFound();
-            }
-
-            var value = new SqlParameter("value", propertyValue);
-
-            var list = await _context.Invoices
-                .FromSqlRaw($"SELECT * FROM Invoices WHERE {propertyName} = @value", value)
-                .ToListAsync();
-            return list;
-        }
-
-        [HttpGet]
-        [Route("ids")]
-        public ActionResult<IEnumerable<Guid>> GetInvoicesIds(string status)
-        {
-            if (_context.Invoices == null)
-            {
-                return NotFound();
-            }
-
-            var result = _context.Database
-                .SqlQuery<Guid>($"SELECT Id FROM Invoices WHERE Status = {status}")
-                .ToList();
-            return result;
-        }
-
-
 
         // GET: api/Invoices/5
         [HttpGet("{id}")]
@@ -161,18 +62,8 @@ namespace BasicEfCoreDemo.Controllers
             {
                 return NotFound();
             }
-            _logger.LogInformation($"Invoice {id} is loading from the database.");
             var invoice = await _context.Invoices.FindAsync(id);
-            _logger.LogInformation($"Invoice {invoice?.Id} is loaded from the database.");
 
-            _logger.LogInformation($"Invoice {id} is loading from the context.");
-            invoice = await _context.Invoices.FindAsync(id);
-            _logger.LogInformation($"Invoice {invoice?.Id} is loaded from the context.");
-
-            invoice = await _context.Invoices.FirstOrDefaultAsync(x => x.Id == id);
-
-            // Use no-tracking query to improve performance.
-            // var invoice = await _context.Invoices.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
             if (invoice == null)
             {
                 return NotFound();
@@ -191,48 +82,25 @@ namespace BasicEfCoreDemo.Controllers
                 return BadRequest();
             }
 
-            // The below code will update all the fields
             //_context.Entry(invoice).State = EntityState.Modified;
-            //try
-            //{
-            //    await _context.SaveChangesAsync();
-            //}
-            //catch (DbUpdateConcurrencyException)
-            //{
-            //    if (!InvoiceExists(id))
-            //    {
-            //        return NotFound();
-            //    }
-            //    else
-            //    {
-            //        throw;
-            //    }
-            //}
 
-            // The below code will update only the fields that have changed
-            // var invoiceToUpdate = await _context.Invoices.FindAsync(id);
-            // if (invoiceToUpdate == null)
-            // {
-            //     return NotFound();
-            // }
-            // invoiceToUpdate.InvoiceNumber = invoice.InvoiceNumber;
-            // invoiceToUpdate.ContactName = invoice.ContactName;
-            // invoiceToUpdate.Description = invoice.Description;
-            // invoiceToUpdate.Amount = invoice.Amount;
-            // invoiceToUpdate.InvoiceDate = invoice.InvoiceDate;
-            // invoiceToUpdate.DueDate = invoice.DueDate;
-            // invoiceToUpdate.Status = invoice.Status;
-
-            // A better way to update only the fields that have changed
-            var invoiceToUpdate = await _context.Invoices.FindAsync(id);
-            if (invoiceToUpdate == null)
-            {
-                return NotFound();
-            }
-            // Update only the properties that have changed
-            _context.Entry(invoiceToUpdate).CurrentValues.SetValues(invoice);
             try
             {
+                var invoiceToUpdate = await _context.Invoices.FindAsync(id);
+                if (invoiceToUpdate == null)
+                {
+                    return NotFound();
+                }
+                // invoiceToUpdate.InvoiceNumber = invoice.InvoiceNumber;
+                // invoiceToUpdate.ContactName = invoice.ContactName;
+                // invoiceToUpdate.Description = invoice.Description;
+                // invoiceToUpdate.Amount = invoice.Amount;
+                // invoiceToUpdate.InvoiceDate = invoice.InvoiceDate;
+                // invoiceToUpdate.DueDate = invoice.DueDate;
+                // invoiceToUpdate.Status = invoice.Status;
+
+                // Update only the properties that have changed
+                _context.Entry(invoiceToUpdate).CurrentValues.SetValues(invoice);
                 await _context.SaveChangesAsync();
             }
             catch (DbUpdateConcurrencyException)
@@ -241,21 +109,13 @@ namespace BasicEfCoreDemo.Controllers
                 {
                     return NotFound();
                 }
-
-                throw;
+                else
+                {
+                    throw;
+                }
             }
 
             return NoContent();
-        }
-
-        [HttpPut]
-        [Route("status/overdue")]
-        public async Task<ActionResult> UpdateInvoicesStatusAsOverdue(DateTime date)
-        {
-            var result = await _context.Invoices
-                .Where(i => i.InvoiceDate < date && i.Status == InvoiceStatus.AwaitPayment)
-                .ExecuteUpdateAsync(s => s.SetProperty(x => x.Status, InvoiceStatus.Overdue));
-            return Ok();
         }
 
         // POST: api/Invoices
@@ -267,9 +127,8 @@ namespace BasicEfCoreDemo.Controllers
             {
                 return Problem("Entity set 'InvoiceDbContext.Invoices'  is null.");
             }
-
             _context.Invoices.Add(invoice);
-            // The above line is equivalent to the following two lines:
+            // The preceding code is equivalent to the following code:
             //_context.Entry(invoice).State = EntityState.Added;
             await _context.SaveChangesAsync();
 
@@ -284,28 +143,14 @@ namespace BasicEfCoreDemo.Controllers
             {
                 return NotFound();
             }
+            var invoice = await _context.Invoices.FindAsync(id);
+            if (invoice == null)
+            {
+                return NotFound();
+            }
 
-            // var invoice = await _context.Invoices.FindAsync(id);
-            // if (invoice == null)
-            // {
-            //     return NotFound();
-            // }
-
-            // _context.Invoices.Remove(invoice);
-            // The above line is equivalent to the following two lines:
-            //_context.Entry(invoice).State = EntityState.Deleted;
-
-            // The following way does not need to find the entity first.
-            // var invoice = new Invoice { Id = id };
-            // _context.Invoices.Remove(invoice);
-
-            //await _context.SaveChangesAsync();
-
-            // Or you can use ExecuteSql method to execute a raw SQL query.
-            //await _context.Database.ExecuteSqlAsync($"DELETE FROM Invoices WHERE Id = {id}");
-
-            // Another way to delete an entity is to use the `ExecuteDeletAsync` method. It is available from EF Core 7.0.
-            await _context.Invoices.Where(x => x.Id == id).ExecuteDeleteAsync();
+            _context.Invoices.Remove(invoice);
+            await _context.SaveChangesAsync();
 
             return NoContent();
         }
--- samples/chapter5/BasicEfCoreDemo/Data/InvoiceDbContext.cs
+++ samples/chapter5/BasicEfCoreDemo/Data/InvoiceDbContext.cs
@@ -1,12 +1,12 @@
 using BasicEfCoreDemo.Models;
+
 using Microsoft.EntityFrameworkCore;
 
 namespace BasicEfCoreDemo.Data;
 
 public class InvoiceDbContext : DbContext
 {
-    public InvoiceDbContext(DbContextOptions<InvoiceDbContext> options)
-        : base(options)
+    public InvoiceDbContext(DbContextOptions<InvoiceDbContext> options) : base(options)
     {
     }
 
@@ -50,7 +50,25 @@ public class InvoiceDbContext : DbContext
                 Status = InvoiceStatus.Draft
             });
 
-        // Use extension methods to configure the model
+        // Configure the entity
+        // modelBuilder.Entity<Invoice>(b =>
+        // {
+        //     b.ToTable("Invoices");
+        //     b.HasKey(i => i.Id);
+        //     b.Property(p => p.Id).HasColumnName("Id");
+        //     b.Property(p => p.InvoiceNumber).HasColumnName("InvoiceNumber").HasColumnType("varchar(32)").IsRequired();
+        //     b.Property(p => p.ContactName).HasColumnName("ContactName").HasMaxLength(32).IsRequired();
+        //     b.Property(p => p.Description).HasColumnName("Description").HasMaxLength(256);
+        //     // b.Property(p => p.Amount).HasColumnName("Amount").HasColumnType("decimal(18,2)").IsRequired();
+        //     b.Property(p => p.Amount).HasColumnName("Amount").HasPrecision(18, 2);
+        //     b.Property(p => p.InvoiceDate).HasColumnName("InvoiceDate").HasColumnType("datetimeoffset").IsRequired();
+        //     b.Property(p => p.DueDate).HasColumnName("DueDate").HasColumnType("datetimeoffset").IsRequired();
+        //     b.Property(p => p.Status).HasColumnName("Status").HasMaxLength(16).HasConversion(
+        //             v => v.ToString(),
+        //             v => (InvoiceStatus)Enum.Parse(typeof(InvoiceStatus), v));
+        // });
+
+        // Use extension methods to configure the entity
         //modelBuilder.ConfigureInvoice();
 
         // Use IEntityTypeConfiguration<TEntity> interface
